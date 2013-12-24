db3e856fae1d32059a5e80691390a6d8dddbbc08
James Unger <junger@muteswan.org>
2013-12-24 14:33:35 -0500

    Add rudimentary post/get for /user endpoint using tiedot DB.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"encoding/json"
+	"io"
 	tiedot "github.com/HouzuoGuo/tiedot/db"
 )
 
@@ -78,14 +79,76 @@ func openDatabase() *tiedot.DB {
 	if err := db.Create("Users"); err != nil {
 		fmt.Println("Collection Users already created.")
         }
-	users := db.Use("Users")
-	docID, err := users.Insert(map[string]interface{}{"First": "firstname-db", "Last": "lastname-db", "Username": "username-db", "Address": "Address line db", "Email": "email@example.com"})
-	if err != nil {
-		fmt.Println("Failed to insert dummy user.")
-	}
-	fmt.Println("docID is ", docID)
+	//users := db.Use("Users")
+	//docID, err := users.Insert(map[string]interface{}{"First": "firstname-db", "Last": "lastname-db", "Username": "username-db", "Address": "Address line db", "Email": "email@example.com"})
+	//if err != nil {
+	//	fmt.Println("Failed to insert dummy user.")
+	//}
+	//fmt.Println("docID is ", docID)
 
 	return db
+}
+
+func postUsers(w http.ResponseWriter, r *http.Request, db *tiedot.DB) {
+	var usersCollection []User
+	body := make([]byte,r.ContentLength)
+	_, err := io.ReadFull(r.Body,body)
+        if err != nil {
+                http.Error(w,"Error reading stream.",500)
+                return
+        }
+	
+
+       	err = json.Unmarshal(body, &usersCollection)
+	if err != nil {
+		http.Error(w,"Failed to parse JSON.",500)
+		return
+	}
+
+	users := db.Use("Users")
+	for i := range usersCollection {
+		docID, err := users.Insert(usersCollection[i])
+		if err != nil {
+	       		fmt.Println("Failed to insert user: ", usersCollection[i])
+		}
+		fmt.Println("Added user ", docID)
+		
+	}
+
+	return
+}
+
+func fetchUsers(w http.ResponseWriter, r *http.Request, db *tiedot.DB) {
+	users := db.Use("Users")
+
+	//queryStr := `[{"eq": "username-db", "in": ["Username"]}]`
+	queryStr := `[{"c": ["all"]}]`
+       	var query interface{}
+	var usersCollection []User
+
+       	json.Unmarshal([]byte(queryStr), &query)
+	queryResult := make(map[uint64]struct{})
+
+	if err := tiedot.EvalQueryV2(query, users, &queryResult); err != nil {
+               	panic(err)
+       	}
+
+
+	fmt.Println(queryResult)
+	for id := range queryResult {
+		user := &User{};
+		users.Read(id,&user)
+               	fmt.Printf("data %s\n", user.Username)
+		usersCollection = append(usersCollection,*user)
+       	}
+
+
+	
+        usersBytes, err := json.Marshal(usersCollection)
+	if err != nil {
+		fmt.Println("Error marshaling json ", err)
+	}
+        w.Write(usersBytes)
 }
 
 func main() {
@@ -104,14 +167,18 @@ func main() {
 	staticRoutes = appendStaticRoute(staticRoutes, staticDir)
 
 
-	db := openDatabase()
+	db := openDatabase() 
 	fmt.Println(db)
 
 
 	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-                user := &User{First: "firstname", Last: "lastname", Username: "username", Address: "address line", Email: "email@example.com"}
-                userBytes, _ := json.Marshal(user)
-                w.Write(userBytes)
+
+		if r.Method == "GET" {
+			fetchUsers(w,r,db)
+		} else {
+			postUsers(w,r,db)
+		}
+
         })
 	
 
